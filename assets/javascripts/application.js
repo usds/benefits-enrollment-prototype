a24f257e64f467248eb4f0de1d25caad3a42bede
Jared Cunha <jcunha@omb.eop.gov>
2016-12-05 09:15:23 -0500

    add the shims

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -4,8 +4,10 @@
 
 // Vender JS
 -{% include_relative vendor/jquery.js %}
+-{% include_relative vendor/polyfiller.js %}
 
 // Scripts
+-{% include_relative scripts/shim.js %}
 -{% include_relative scripts/session_storage.js %}
 -{% include_relative scripts/prep_storage.js %}
 -{% include_relative scripts/auto_advance_fields.js %}
